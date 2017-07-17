7ed88aad7a59798bcc6843012bf7466c2d448440
divastavisca <dagarwal@tavisca.com>
2017-07-17 11:24:56 +0530

    creating console UI

--- TrainingSchedulingReporting/Program.cs
+++ TrainingSchedulingReporting/Program.cs
@@ -14,12 +14,37 @@ namespace TrainingSchedulingReporting
             {
                 Console.WriteLine("Enter your choice");
                 Console.WriteLine("(1)Tasks\n(2)Schedulling\n(3)Reporting\n(4)Exit");
-                int choice;
+                int choice,innerchoice;
+                bool brk;
                 if (Int32.TryParse(Console.ReadLine(), out choice))
                 {
                     switch(choice)
                     {
                         case 1:
+                            brk = true;
+                            while (brk)
+                            {
+                                Console.WriteLine("(1)Register a new task\n(2)View tasks (Track)\n(3)Modify a task\n(4)Go back");
+                                if (Int32.TryParse(Console.ReadLine(), out innerchoice))
+                                {
+                                    switch(innerchoice)
+                                    {
+                                        case 1:
+                                            break;
+                                        case 2:
+                                            break;
+                                        case 3:
+                                            break;
+                                        case 4:
+                                            brk = false;
+                                            break;
+                                        default:
+                                            Console.WriteLine("Enter a valid choice\n");
+                                            break;
+                                    }
+                                }
+                                else Console.WriteLine("Enter a valid choice\n");
+                            }
                             break;
                         case 2:
                             break;
